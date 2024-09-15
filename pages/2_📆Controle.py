edc17c08db19d2f04495d12cc549687636c3fc2e
lucascarvalhosantiagogmail <lucascarvalhosnt@gmail.com>
2024-09-15 18:00:53 -0300

    adicionando

--- pages/2_📆Controle.py
+++ pages/2_📆Controle.py
@@ -14,6 +14,7 @@ st.set_page_config(
     page_icon= "https://img1.wsimg.com/isteam/ip/0cdba6f5-2fc0-4aaf-b030-d8df637187a2/blob-46e0c21.png/:/rs=w:134,h:100,cg:true,m/cr=w:134,h:100/qt=q:100/ll",
     layout="wide"
 )
+st.logo("https://img1.wsimg.com/isteam/ip/0cdba6f5-2fc0-4aaf-b030-d8df637187a2/blob-46e0c21.png/:/rs=w:134,h:100,cg:true,m/cr=w:134,h:100/qt=q:100/ll")
 
 # Função JavaScript para manipular o localStorage
 local_storage_script = """
